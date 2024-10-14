5a23b9079f4e221414bb8bf3d6701d227d0fc35f
Julian Böhne <julian.boehne@web.de>
2024-10-14 11:12:30 +0200

    feat: add grid to playserver

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -3,6 +3,11 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import aview.TUI
+import core.controller.ControllerInterface
+import core.controller.controllerImpl.{APIController, Controller, KafkaConsumer}
+import core.model.gridImpl.{Grid, Ship, ShipContainer, Shots}
+import play.twirl.api.Html
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
@@ -10,6 +15,10 @@ import play.api.mvc._
  */
 @Singleton
 class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+
+  private val grid: Grid = Grid(10, Shots(Vector[Int](), Vector[Int]()), ShipContainer(Vector[Ship]()))
+  private val controller = new Controller(grid)
+  private val tui: TUI = new TUI(controller)
 
   /**
    * Create an Action to render an HTML page.
@@ -19,6 +28,10 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
    * a path of `/`.
    */
   def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+    val text = controller.toString
+    val htmlText = s"<pre>${text.replace("\n", "<br>")}</pre>"
+    val html: Html = Html(htmlText)
+    Ok(views.html.main(title = "Battleship")(content = html))
+//    Ok(views.html.index())
   }
 }
