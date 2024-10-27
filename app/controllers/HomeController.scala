6a9df2f9736ca8dabf871640b336ea57060fe7fc
Julian Böhne <julian.boehne@web.de>
2024-10-27 13:23:19 +0100

    feat: ship adding

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -49,14 +49,18 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
     Ok(views.html.menu(title = "isLost")(content = html))
   }
 
-  def GridShipToString() = Action { implicit request: Request[AnyContent] =>
-//    val text = controller.GridShipToString
-//    val htmlText = s"<pre>${text.replace("\n", "<br>")}</pre>"
-//    val html: Html = Html(htmlText)
+  def addShips1() = Action { implicit request: Request[AnyContent] =>
     val size = controller.grid.size
     val board = controller.grid.getBoard
 
-    Ok(views.html.shipfield(title = "Battleship Grid")(size = size)(board = board))
+    Ok(views.html.shipfield1(title = "Battleship Grid")(size = size)(board = board))
+  }
+
+  def addShips2() = Action { implicit request: Request[AnyContent] =>
+    val size = controller.grid.size
+    val board = controller.grid.getBoard
+
+    Ok(views.html.shipfield2(title = "Battleship Grid")(size = size)(board = board))
   }
 
   def undo() = Action { implicit request: Request[AnyContent] =>
@@ -160,6 +164,65 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
         Ok(Json.obj("status" -> "success", "message" -> s"Player2 Shot added at ($x, $y)"))
       case _ =>
         BadRequest(Json.obj("status" -> "error", "message" -> "Invalid or missing coordinates"))
+    }
+  }
+
+  def player1AddShip(): Action[AnyContent] = Action { implicit request =>
+
+    // Versuch, die Koordinaten aus verschiedenen Eingabemethoden zu lesen
+    val cords1 = request.body.asFormUrlEncoded.flatMap(_.get("first").flatMap(_.headOption)).map(_.toString)
+      .orElse(request.body.asJson.flatMap(json => (json \ "first").asOpt[String]))
+      .orElse(request.getQueryString("first").map(_.toString))
+
+    val cords2 = request.body.asFormUrlEncoded.flatMap(_.get("second").flatMap(_.headOption)).map(_.toString)
+      .orElse(request.body.asJson.flatMap(json => (json \ "second").asOpt[String]))
+      .orElse(request.getQueryString("second").map(_.toString))
+
+    (cords1, cords2) match {
+      case (Some(first), Some(second)) =>
+        controller.state = controller.player1
+        val x1 =controller.getX(first)
+        val y1 = controller.getY(first)
+        val x2 =controller.getX(second)
+        val y2 = controller.getY(second)
+        val check = controller.checkShip(x1, y1, x2, y2)
+
+        if (check) {
+          controller.set(x1, y1, x2, y2)
+          Ok(Json.obj("status" -> "success", "message" -> s"Player1 Ship added at ($x1, $y1) to ($x2, $y2)"))
+        } else {
+          BadRequest(Json.obj("status" -> "error", "message" -> "Invalid or missing coordinates"))
+        }
+    }
+  }
+
+
+  def player2AddShip(): Action[AnyContent] = Action { implicit request =>
+
+    // Versuch, die Koordinaten aus verschiedenen Eingabemethoden zu lesen
+    val xCoord = request.body.asFormUrlEncoded.flatMap(_.get("first").flatMap(_.headOption)).map(_.toString)
+      .orElse(request.body.asJson.flatMap(json => (json \ "first").asOpt[String]))
+      .orElse(request.getQueryString("first").map(_.toString))
+
+    val yCoord = request.body.asFormUrlEncoded.flatMap(_.get("second").flatMap(_.headOption)).map(_.toString)
+      .orElse(request.body.asJson.flatMap(json => (json \ "second").asOpt[String]))
+      .orElse(request.getQueryString("second").map(_.toString))
+
+    (xCoord, yCoord) match {
+      case (Some(first), Some(second)) =>
+        controller.state = controller.player2
+        val x1 =controller.getX(first)
+        val y1 = controller.getY(first)
+        val x2 =controller.getX(second)
+        val y2 = controller.getY(second)
+        val check = controller.checkShip(x1, y1, x2, y2)
+
+        if (check) {
+          controller.set(x1, y1, x2, y2)
+          Ok(Json.obj("status" -> "success", "message" -> s"Player1 Ship added at ($x1, $y1) to ($x2, $y2)"))
+        } else {
+          BadRequest(Json.obj("status" -> "error", "message" -> "Invalid or missing coordinates"))
+        }
     }
   }
 
