5c377511f867f2ffb1187610428824eeddc40187
Fabian Meyer <meyer.fa@gmx.de>
2024-12-02 16:31:51 +0100

    added field blur

--- public/javascripts/field.js
+++ public/javascripts/field.js
@@ -25,6 +25,20 @@ $(document).ready(function () {
     });
 });
 
+function updateFieldVisibility(activePlayer) {
+    const player1Field = document.getElementById('player1-field');
+    const player2Field = document.getElementById('player2-field');
+
+    if (activePlayer === 2) {
+        // Spieler 1 ist aktiv, zielt auf Spieler 2's Feld
+        player1Field.classList.add('inactive-field');
+        player2Field.classList.remove('inactive-field');
+    } else {
+        // Spieler 2 ist aktiv, zielt auf Spieler 1's Feld
+        player2Field.classList.add('inactive-field');
+        player1Field.classList.remove('inactive-field');
+    }
+}
 
 function isValid(input) {
     let xhr = new XMLHttpRequest();
