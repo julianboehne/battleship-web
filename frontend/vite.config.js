4d9564146d11e6c8e114386ccec188ae943a4f2d
Fabian Meyer <meyer.fa@gmx.de>
2025-01-06 17:48:30 +0100

    added gh pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/battleship-web/',
   plugins: [
     vue(),
     vueDevTools(),
