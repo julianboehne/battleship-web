74b5db227b19d2ab71f3559d06e1d1656577d56a
Julian Böhne <julian.boehne@web.de>
2025-01-20 18:30:31 +0100

    add auth

---
+++ frontend/src/components/RegisterPage.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="box">
+    <h1>Register or Sign in</h1>
+    <form>
+      <input type="text" placeholder="Email" v-model="email" />
+      <input type="password" placeholder="Password" v-model="password" />
+
+      <div id="btn-group">
+        <button @click="register">Register</button>
+        <button @click="login">Sign in</button>
+      </div>
+    </form>
+  </div>
+</template>
+<style scoped>
+#box {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+form {
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+  gap: 5px;
+}
+#btn-group {
+  display: flex;
+  gap: 10px;
+  justify-content: center;
+}
+button {
+  width: calc(50% - 10px);
+}
+</style>
+<script>
+import { auth } from '../firebaseConfig'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+export default {
+  name: 'RegisterPage',
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    register() {
+      createUserWithEmailAndPassword(auth, this.email, this.password)
+          .then((data) => {
+            console.log(data)
+            console.log('Successfully registered!');
+            this.$router.push('/')
+          })
+          .catch(error => {
+            console.log(error.code)
+            alert(error.message);
+          });
+    },
+    login() {
+      signInWithEmailAndPassword(auth, this.email, this.password)
+          .then((data) => {
+            console.log(data);
+            console.log('Successfully logged in!');
+            this.$router.push('/');
+          })
+          .catch(error => {
+            console.log(error.code);
+            alert(error.message);
+          });
+    }
+  }
+}
+</script>
---
+++ frontend/src/firebaseConfig.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCeanO8Co11tQqG61WRqkn8sgOJ3N535_s",
+    authDomain: "battleship-web-ee919.firebaseapp.com",
+    projectId: "battleship-web-ee919",
+    storageBucket: "battleship-web-ee919.firebasestorage.app",
+    messagingSenderId: "747272421844",
+    appId: "1:747272421844:web:8435ffb685f0f41ec39641",
+    measurementId: "G-4X1BNT6HT2"
+};
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export { app, auth }
